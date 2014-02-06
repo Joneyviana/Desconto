4725428aa7bc54268c8f96ec9dc6c73753992fd6
Joneyviana <arcistiel@gmail.com>
2014-02-06 10:57:39 -0200

    Criação  do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Desconto::Application.config.secret_key_base = '387bb5c82a76079d83d37ec6f66f868ca82b18908e9985d31f1b5b4fe87af773941ebed48d329819401e827b7c46fe67532a0d0b85b3f85d64219b3bc465b2ae'
